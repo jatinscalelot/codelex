899450e5a91673a54f9b6bfad3be0f29ed30b78c
Jatin Dhameliya <jatin.scalelot@gmail.com>
2023-03-09 16:02:40 +0530

    updated all apps apis for user

--- public/javascripts/controllers/apps.controller.js
+++ public/javascripts/controllers/apps.controller.js
@@ -1,6 +1,7 @@
 app.controller("AppsController", ($scope, $http, HelperService) => {
+    $scope.appId = 0;
     $scope.appdata = {
-        acccount_name: '', 
+        acccount_name: '',
         package_name: '',
         banner_ADS: '',
         native_ADS: '',
@@ -12,22 +13,23 @@ app.controller("AppsController", ($scope, $http, HelperService) => {
         rewarded_ADS: '',
         web_URL: '',
         privacy_policy: '',
-        adx_status_1 : true,
-        adx_status_2 : true,
-        adx_status_3 : true,
-        vpn_status : true,
-        web_url_status : true,
-        all_status : true 
+        frequency: '',
+        adx_status_1: true,
+        adx_status_2: true,
+        adx_status_3: true,
+        vpn_status: true,
+        web_url_status: true,
+        all_status: true
     };
     $scope.addNew = function () {
         window.location.href = "/apps/create";
     }
     $scope.create = function () {
         $http({
-            url: BASE_URL+'apps',
+            url: BASE_URL + 'apps',
             method: "POST",
             cache: false,
-            data: {appdata : $scope.appdata},
+            data: { appdata: $scope.appdata },
             headers: {
                 "Content-Type": "application/json; charset=UTF-8",
             },
@@ -49,4 +51,207 @@ app.controller("AppsController", ($scope, $http, HelperService) => {
             }
         );
     }
+    $scope.updateApp = function () {
+        if($scope.appId != '' && $scope.appId != 0){
+            $http({
+                url: BASE_URL + 'apps/update',
+                method: "POST",
+                cache: false,
+                data: { appid : $scope.appId, appdata: $scope.appdata },
+                headers: {
+                    "Content-Type": "application/json; charset=UTF-8",
+                },
+            }).then(
+                function (response) {
+                    if (response.data.IsSuccess == true && response.data.Data != 0) {
+                        swal("Congo.", "App data saved successfully...", "success");
+                        window.location.href = "/apps";
+                    } else {
+                        swal("Oops", response.data.Message, "error");
+                    }
+                },
+                function (error) {
+                    console.log(error);
+                    if (error.status == 401) {
+                        window.location.href = AUTO_LOGOUT;
+                    }
+                    console.error("Something Went Wrong! try again");
+                }
+            );
+        }
+    }
+    $scope.cancleBtn = () => {
+        window.location.href = "/apps";
+    }
+    $scope.page = 1;
+    $scope.limit = "10";
+    $scope.search = "";
+    $scope.apps = [];
+    $scope.pageNumberList = [];
+    $scope.getapps = () => {
+        let request = { page: $scope.page, limit: $scope.limit, search: $scope.search };
+        $http({
+            url: BASE_URL + "apps/list",
+            method: "POST",
+            data: request,
+            cache: false,
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8",
+            },
+        }).then(
+            function (response) {
+                if (response.data.IsSuccess == true) {
+                    $scope.apps = response.data.Data;
+                    $scope.pageNumberList = HelperService.paginator($scope.apps.totalPages, $scope.page, 9);
+                }
+            },
+            function (error) {
+                $('#loadingdiv').hide();
+                console.log(error);
+                if (error.status == 401) {
+                    window.location.href = AUTO_LOGOUT;
+                }
+            }
+        );
+    }
+    $scope.getapps();
+    $scope.onSearch = () => {
+        $scope.page = 1;
+        $scope.getapps();
+    };
+    $scope.$watch("page", () => { $scope.getapps() });
+    $scope.onLimitChange = () => { $scope.page = 1; $scope.getapps(); };
+    $scope.changeadx_status_1 = (mapp) => {
+        $http({
+            url: BASE_URL + "apps/changeadx_status_1",
+            method: "POST",
+            cache: false,
+            data: { aid: mapp._id, adx_status_1: mapp.adx_status_1 },
+            headers: { "Content-Type": "application/json; charset=UTF-8" },
+        }).then(
+            function (response) {
+                if (response.data.IsSuccess == true) {
+                    $scope.getapps();
+                }
+            }
+        );
+    };
+    $scope.changeadx_status_2 = (mapp) => {
+        $http({
+            url: BASE_URL + "apps/changeadx_status_2",
+            method: "POST",
+            cache: false,
+            data: { aid: mapp._id, adx_status_2: mapp.adx_status_2 },
+            headers: { "Content-Type": "application/json; charset=UTF-8" },
+        }).then(
+            function (response) {
+                if (response.data.IsSuccess == true) {
+                    $scope.getapps();
+                }
+            }
+        );
+    };
+    $scope.changeadx_status_3 = (mapp) => {
+        $http({
+            url: BASE_URL + "apps/changeadx_status_3",
+            method: "POST",
+            cache: false,
+            data: { aid: mapp._id, adx_status_3: mapp.adx_status_3 },
+            headers: { "Content-Type": "application/json; charset=UTF-8" },
+        }).then(
+            function (response) {
+                if (response.data.IsSuccess == true) {
+                    $scope.getapps();
+                }
+            }
+        );
+    };
+    $scope.changevpn_status = (mapp) => {
+        $http({
+            url: BASE_URL + "apps/changevpn_status",
+            method: "POST",
+            cache: false,
+            data: { aid: mapp._id, vpn_status: mapp.vpn_status },
+            headers: { "Content-Type": "application/json; charset=UTF-8" },
+        }).then(
+            function (response) {
+                if (response.data.IsSuccess == true) {
+                    $scope.getapps();
+                }
+            }
+        );
+    };
+    $scope.changeweb_url_status = (mapp) => {
+        $http({
+            url: BASE_URL + "apps/changeweb_url_status",
+            method: "POST",
+            cache: false,
+            data: { aid: mapp._id, web_url_status: mapp.web_url_status },
+            headers: { "Content-Type": "application/json; charset=UTF-8" },
+        }).then(
+            function (response) {
+                if (response.data.IsSuccess == true) {
+                    $scope.getapps();
+                }
+            }
+        );
+    };
+    $scope.changeall_status = (mapp) => {
+        $http({
+            url: BASE_URL + "apps/changeall_status",
+            method: "POST",
+            cache: false,
+            data: { aid: mapp._id, all_status: mapp.all_status },
+            headers: { "Content-Type": "application/json; charset=UTF-8" },
+        }).then(
+            function (response) {
+                if (response.data.IsSuccess == true) {
+                    $scope.getapps();
+                }
+            }
+        );
+    };
+    $scope.removeapp = (appid) => {
+        $http({
+            url: BASE_URL + "apps/removeapp",
+            method: "POST",
+            cache: false,
+            data: { aid: appid },
+            headers: { "Content-Type": "application/json; charset=UTF-8" },
+        }).then(
+            function (response) {
+                if (response.data.IsSuccess == true) {
+                    swal("Congo.", "App data removed successfully...", "success");
+                    $scope.getapps();
+                }
+            }
+        );
+    };
+    $scope.onEditLoad = () => {
+        let aid = HelperService.queryString('aid');
+        if (aid != 0 && aid != null && aid != '' && aid != undefined) {
+            $scope.appId = aid;
+            $http({
+                url: BASE_URL + "apps/getone",
+                method: "POST",
+                cache: false,
+                data: { aid: aid },
+                headers: { "Content-Type": "application/json; charset=UTF-8" },
+            }).then(
+                function (response) {
+                    if (response.data.IsSuccess == true && response.data.Data != 0) {
+                        $scope.appdata = response.data.Data;
+                    } else {
+                        window.location.href = "/apps";
+                    }
+                }, function (error) {
+                    console.error(error);
+                    if (error.status == 401) {
+                        window.location.href = AUTO_LOGOUT;
+                    }
+                }
+            );
+        }
+    };
+    $scope.onEditLoad();
 });
--- routes/admin/apps.js
+++ routes/admin/apps.js
@@ -13,10 +13,23 @@ router.get('/', async (req, res) => {
         if (userData && userData.status == true) {
             res.render('apps/list', { title: 'apps' });
         } else {
-            res.render('login', { layout: false, title: 'Express' });
+            res.redirect("/");
         }
     } else {
-        res.render('login', { layout: false, title: 'Express' });
+        res.redirect("/");
+    }
+});
+router.get('/edit', async (req, res) => {
+    if (req.session.userId) {
+        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+        let userData = await primary.model(constants.MODELS.users, userModel).findById(req.session.userId).lean();
+        if (userData && userData.status == true) {
+            res.render('apps/edit', { title: 'apps' });
+        } else {
+            res.redirect("/");
+        }
+    } else {
+        res.redirect("/");
     }
 });
 router.post('/', async (req, res) => {
@@ -48,6 +61,219 @@ router.get('/create', async (req, res) => {
         }
     } else {
         res.render('login', { layout: false, title: 'Express' });
+    }
+});
+router.post('/list', async (req, res) => {
+    if (req.session.userId) {
+        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+        const { limit, page, search } = req.body;
+        let regex = new RegExp(search, 'i');
+        primary.model(constants.MODELS.apps, appModel).paginate({
+            $or: [{ acccount_name: regex }, { package_name: regex }, { banner_ADS: regex }, { native_ADS: regex }, { VPN_key: regex }, { priority: regex }, { application_name: regex },
+                { app_open_ads: regex },{ interstitial_ADS: regex },{ rewarded_ADS: regex },{ web_URL: regex },{ privacy_policy: regex },{ frequency: regex }
+            ]
+        }, {
+            page,
+            limit: parseInt(limit),
+            sort: { _id : -1 },
+            lean: true
+        }).then((appslist) => {
+            return responseManager.onSuccess("Apps List", appslist, res);
+        }).catch((err) => {
+            return responseManager.onError(err, res);
+        });
+    } else {
+        return responseManager.unauthorisedRequest(res);
+    }
+});
+router.post('/changeadx_status_1', async (req, res) => {
+    if (req.session.userId) {
+        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+        const { aid, adx_status_1 } = req.body;
+        let existingData = await primary.model(constants.MODELS.apps, appModel).findById(aid).lean();
+        if(existingData){
+            if(existingData.adx_status_1 == true){
+                await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {adx_status_1 : false, all_status : false});
+                return responseManager.onSuccess("status updated", 1, res);
+            }else{
+                if(existingData.adx_status_2 == true && existingData.adx_status_3 == true && existingData.vpn_status == true && existingData.web_url_status == true){
+                    await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {adx_status_1 : true, all_status : true});
+                    return responseManager.onSuccess("status updated", 1, res);
+                }else{
+                    await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {adx_status_1 : true});
+                    return responseManager.onSuccess("status updated", 1, res);
+                }                
+            }
+        }else{
+            return responseManager.unauthorisedRequest(res);
+        } 
+    }else{
+        return responseManager.unauthorisedRequest(res);
+    }
+});
+router.post('/changeadx_status_2', async (req, res) => {
+    if (req.session.userId) {
+        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+        const { aid, adx_status_2 } = req.body;
+        let existingData = await primary.model(constants.MODELS.apps, appModel).findById(aid).lean();
+        if(existingData){
+            if(existingData.adx_status_2 == true){
+                await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {adx_status_2 : false, all_status : false});
+                return responseManager.onSuccess("status updated", 1, res);
+            }else{
+                if(existingData.adx_status_1 == true && existingData.adx_status_3 == true && existingData.vpn_status == true && existingData.web_url_status == true){
+                    await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {adx_status_2 : true, all_status : true});
+                    return responseManager.onSuccess("status updated", 1, res);
+                }else{
+                    await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {adx_status_2 : true});
+                    return responseManager.onSuccess("status updated", 1, res);
+                }                
+            }
+        }else{
+            return responseManager.unauthorisedRequest(res);
+        } 
+    }else{
+        return responseManager.unauthorisedRequest(res);
+    }
+});
+router.post('/changeadx_status_3', async (req, res) => {
+    if (req.session.userId) {
+        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+        const { aid, adx_status_3 } = req.body;
+        let existingData = await primary.model(constants.MODELS.apps, appModel).findById(aid).lean();
+        if(existingData){
+            if(existingData.adx_status_3 == true){
+                await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {adx_status_3 : false, all_status : false});
+                return responseManager.onSuccess("status updated", 1, res);
+            }else{
+                if(existingData.adx_status_1 == true && existingData.adx_status_2 == true && existingData.vpn_status == true && existingData.web_url_status == true){
+                    await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {adx_status_3 : true, all_status : true});
+                    return responseManager.onSuccess("status updated", 1, res);
+                }else{
+                    await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {adx_status_3 : true});
+                    return responseManager.onSuccess("status updated", 1, res);
+                }                
+            }
+        }else{
+            return responseManager.unauthorisedRequest(res);
+        } 
+    }else{
+        return responseManager.unauthorisedRequest(res);
+    }
+});
+router.post('/changevpn_status', async (req, res) => {
+    if (req.session.userId) {
+        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+        const { aid, vpn_status } = req.body;
+        let existingData = await primary.model(constants.MODELS.apps, appModel).findById(aid).lean();
+        if(existingData){
+            if(existingData.vpn_status == true){
+                await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {vpn_status : false, all_status : false});
+                return responseManager.onSuccess("status updated", 1, res);
+            }else{
+                if(existingData.adx_status_1 == true && existingData.adx_status_2 == true && existingData.adx_status_3 == true && existingData.web_url_status == true){
+                    await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {vpn_status : true, all_status : true});
+                    return responseManager.onSuccess("status updated", 1, res);
+                }else{
+                    await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {vpn_status : true});
+                    return responseManager.onSuccess("status updated", 1, res);
+                }                
+            }
+        }else{
+            return responseManager.unauthorisedRequest(res);
+        } 
+    }else{
+        return responseManager.unauthorisedRequest(res);
+    }
+});
+router.post('/changeweb_url_status', async (req, res) => {
+    if (req.session.userId) {
+        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+        const { aid, web_url_status } = req.body;
+        let existingData = await primary.model(constants.MODELS.apps, appModel).findById(aid).lean();
+        if(existingData){
+            if(existingData.web_url_status == true){
+                await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {web_url_status : false, all_status : false});
+                return responseManager.onSuccess("status updated", 1, res);
+            }else{
+                if(existingData.adx_status_1 == true && existingData.adx_status_2 == true && existingData.adx_status_3 == true && existingData.vpn_status == true){
+                    await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {web_url_status : true, all_status : true});
+                    return responseManager.onSuccess("status updated", 1, res);
+                }else{
+                    await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {web_url_status : true});
+                    return responseManager.onSuccess("status updated", 1, res);
+                }                
+            }
+        }else{
+            return responseManager.unauthorisedRequest(res);
+        } 
+    }else{
+        return responseManager.unauthorisedRequest(res);
+    }
+});
+router.post('/changeall_status', async (req, res) => {
+    if (req.session.userId) {
+        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+        const { aid, all_status } = req.body;
+        let existingData = await primary.model(constants.MODELS.apps, appModel).findById(aid).lean();
+        if(existingData){
+            if(existingData.all_status == true){
+                await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {adx_status_1 : false, adx_status_2 : false, adx_status_3 : false, vpn_status : false, web_url_status : false, all_status : false});
+                return responseManager.onSuccess("status updated", 1, res);
+            }else{
+                await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(aid, {adx_status_1 : true, adx_status_2 : true, adx_status_3 : true, vpn_status : true, web_url_status : true, all_status : true});
+                return responseManager.onSuccess("status updated", 1, res);        
+            }
+        }else{
+            return responseManager.unauthorisedRequest(res);
+        } 
+    }else{
+        return responseManager.unauthorisedRequest(res);
+    }
+});
+router.post('/removeapp', async (req, res) => {
+    if (req.session.userId) {
+        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+        const { aid } = req.body;
+        let existingData = await primary.model(constants.MODELS.apps, appModel).findById(aid).lean();
+        if(existingData){
+            await primary.model(constants.MODELS.apps, appModel).findByIdAndRemove(aid);
+            return responseManager.onSuccess("App removed", 1, res);
+        }else{
+            return responseManager.unauthorisedRequest(res);
+        } 
+    }else{
+        return responseManager.unauthorisedRequest(res);
+    }
+});
+router.post('/getone', async (req, res) => {
+    if (req.session.userId) {
+        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+        const { aid } = req.body;
+        let existingData = await primary.model(constants.MODELS.apps, appModel).findById(aid).select('-createdBy -updatedBy -timestamp -createdAt -updatedAt -__v').lean();
+        if(existingData){
+            return responseManager.onSuccess("App removed", existingData, res);
+        }else{
+            return responseManager.unauthorisedRequest(res);
+        } 
+    }else{
+        return responseManager.unauthorisedRequest(res);
+    }
+});
+router.post('/update', async (req, res) => {
+    if (req.session.userId) {
+        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+        let userData = await primary.model(constants.MODELS.users, userModel).findById(req.session.userId).lean();
+        if(userData && userData.status == true){
+            const { appid, appdata } = req.body;
+            appdata.updatedBy = new mongoose.Types.ObjectId(userData._id);
+            await primary.model(constants.MODELS.apps, appModel).findByIdAndUpdate(appid, appdata);
+            return responseManager.onSuccess('App added successfully...', { appid: appid }, res);
+        }else{
+            return responseManager.badrequest({message : 'Invalid user to update app, please try again'}, res);
+        }
+    } else {
+        return responseManager.badrequest({message : 'Invalid user to update app, please try again'}, res);
     }
 });
 module.exports = router;
